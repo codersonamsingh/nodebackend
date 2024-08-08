3cc801922e999e7665d922a679f01139c61a8db6
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-08-08 16:34:41 +0530

    index.js running

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,6 +9,9 @@ app.get('/', (req, res) => {
 app.get('/twitter', (req, res) => {
     res.send('sonamsingh')
   })
+app.get('/login', (req, res) => {
+  res,send('<h1>Please login at sonam singh rajput</h1>')
+})  
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
